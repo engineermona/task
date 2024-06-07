08bbc482e4be363607f10c4471fa3a1aeab2ffed
Amir <amir@192.168.1.4>
2024-06-08 00:33:18 +0300

    added quiz 1

---
+++ quiz1.py
@@ -0,0 +1,34 @@
+def employee_system(liste):
+    list=[1,2,3,4,5]
+    print("enter your choise")
+    for i in list:
+        if i==1:
+            input("add new employee")
+            print("enter employee data:")
+            input("enter the name")
+            input("enter the age")
+            input("enter the salary")
+        if i==2:
+            print("f{name} and has age {age} and his salary is {salary}")
+        if i==3:
+
+        if i==4:
+
+        if i==5
+            return
+        if i!=list
+            raise"invalied range try again "            
+        
+employee_system(list)
+    
+
+    
+ 
+     
+     
+
+
+
+    
+
+                
